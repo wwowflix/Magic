da86e7c853e85c1f6a746757d344d1b7c6c7fc95
wwowflix <218474503+wwowflix@users.noreply.github.com>
2025-11-19 09:21:35 +0530

    MAGIC: Batch 2 import shims (requests + charset + annotations)

--- scripts/api.py
+++ scripts/api.py
@@ -1,157 +1,107 @@
+﻿from __future__ import annotations
+
 """
-requests.api
-~~~~~~~~~~~~
+MAGIC shim for scripts.api
 
-This module implements the Requests API.
+Two roles:
 
-:copyright: (c) 2012 by Kenneth Reitz.
-:license: Apache2, see LICENSE for more details.
+1. Small "requests-style" public API:
+   - request(), get(), post(), put(), delete(), head(), options(), patch()
+   - session() → creates a requests-like Session from scripts.sessions
+
+2. Minimal "platformdirs-style" API:
+   - PlatformDirsABC base class that android.py can import.
 """
 
-from . import sessions
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from . import sessions as _sessions
 
 
-def request(method, url, **kwargs):
-    """Constructs and sends a :class:`Request <Request>`.
-
-    :param method: method for the new :class:`Request` object: ``GET``, ``OPTIONS``, ``HEAD``, ``POST``, ``PUT``, ``PATCH``, or ``DELETE``.
-    :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary, list of tuples or bytes to send
-        in the query string for the :class:`Request`.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
-    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
-    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
-        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
-        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content_type'`` is a string
-        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
-        to add for the file.
-    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
-    :param timeout: (optional) How many seconds to wait for the server to send data
-        before giving up, as a float, or a :ref:`(connect timeout, read
-        timeout) <timeouts>` tuple.
-    :type timeout: float or tuple
-    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
-    :type allow_redirects: bool
-    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
-    :param verify: (optional) Either a boolean, in which case it controls whether we verify
-            the server's TLS certificate, or a string, in which case it must be a path
-            to a CA bundle to use. Defaults to ``True``.
-    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
-    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-
-    Usage::
-
-      >>> import requests
-      >>> req = requests.request('GET', 'https://httpbin.org/get')
-      >>> req
-      <Response [200]>
-    """
-
-    # By using the 'with' statement we are sure the session is closed, thus we
-    # avoid leaving sockets open which can trigger a ResourceWarning in some
-    # cases, and look like a memory leak in others.
-    with sessions.Session() as session:
-        return session.request(method=method, url=url, **kwargs)
+def request(method: str, url: str, **kwargs: Any):
+    s = _sessions.Session()
+    return s.request(method=method, url=url, **kwargs)
 
 
-def get(url, params=None, **kwargs):
-    r"""Sends a GET request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary, list of tuples or bytes to send
-        in the query string for the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    return request("get", url, params=params, **kwargs)
+def get(url: str, **kwargs: Any):
+    return request("GET", url, **kwargs)
 
 
-def options(url, **kwargs):
-    r"""Sends an OPTIONS request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    return request("options", url, **kwargs)
+def post(url: str, **kwargs: Any):
+    return request("POST", url, **kwargs)
 
 
-def head(url, **kwargs):
-    r"""Sends a HEAD request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes. If
-        `allow_redirects` is not provided, it will be set to `False` (as
-        opposed to the default :meth:`request` behavior).
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    kwargs.setdefault("allow_redirects", False)
-    return request("head", url, **kwargs)
+def put(url: str, **kwargs: Any):
+    return request("PUT", url, **kwargs)
 
 
-def post(url, data=None, json=None, **kwargs):
-    r"""Sends a POST request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    return request("post", url, data=data, json=json, **kwargs)
+def delete(url: str, **kwargs: Any):
+    return request("DELETE", url, **kwargs)
 
 
-def put(url, data=None, **kwargs):
-    r"""Sends a PUT request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    return request("put", url, data=data, **kwargs)
+def head(url: str, **kwargs: Any):
+    return request("HEAD", url, **kwargs)
 
 
-def patch(url, data=None, **kwargs):
-    r"""Sends a PATCH request.
-
-    :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
-
-    return request("patch", url, data=data, **kwargs)
+def options(url: str, **kwargs: Any):
+    return request("OPTIONS", url, **kwargs)
 
 
-def delete(url, **kwargs):
-    r"""Sends a DELETE request.
+def patch(url: str, **kwargs: Any):
+    return request("PATCH", url, **kwargs)
 
-    :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    """
 
-    return request("delete", url, **kwargs)
+def session() -> "_sessions.Session":
+    return _sessions.Session()
+
+
+@dataclass
+class PlatformDirsABC(ABC):
+    appname: Optional[str] = None
+    appauthor: Optional[str] = None
+    roaming: bool = False
+    ensure_exists: bool = False
+
+    @abstractmethod
+    def user_data_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def site_data_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def user_config_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def site_config_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def user_cache_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def user_state_dir(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def user_log_dir(self) -> str:
+        raise NotImplementedError
+
+
+__all__ = [
+    "request",
+    "get",
+    "post",
+    "put",
+    "delete",
+    "head",
+    "options",
+    "patch",
+    "session",
+    "PlatformDirsABC",
+]
